0c8e0615680cdbfbb4638f172a81fbf38565d8e7
glitterb0mb <srd.dullflame@gmail.com>
2022-04-10 11:27:41 -0400

    forecast restart

--- src/app.js
+++ src/app.js
@@ -47,7 +47,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiKey = "accd6b75554184ea54b4d2360ba258b0";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
