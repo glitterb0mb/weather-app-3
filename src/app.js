855f2d60782dc7b4bd0853a2f6ff3c1d2dad6a77
glitterb0mb <srd.dullflame@gmail.com>
2022-04-10 17:50:33 -0400

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,7 +22,8 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
   let days = ["SUN", "MON", "TUE", "WED"];
@@ -52,6 +53,7 @@ function getForecast(coordinates) {
   let apiKey = "accd6b75554184ea54b4d2360ba258b0";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiURL);
+  axios.get(apiURL).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -121,5 +123,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-displayForecast();
 search("atlanta");
